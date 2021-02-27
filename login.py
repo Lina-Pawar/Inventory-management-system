fb2d49e7744b8a6c47ed831309e94a7a39fd120f
Lina-Pawar <lpawar2911@gmail.com>
2021-02-27 22:40:14 +0530

    commit

--- login.py
+++ login.py
@@ -20,6 +20,7 @@ class User_Login:
         self.regs_b=ImageTk.PhotoImage(Image.open("D:/IMS/icons/button_register.png"))
         self.pass_b=ImageTk.PhotoImage(Image.open("D:/IMS/icons/button_forgot-password (1).png"))
         self.titlebg=ImageTk.PhotoImage(Image.open("D:/IMS/icons/title.png"))
+        self.passbg=ImageTk.PhotoImage(Image.open("D:/IMS/icons/bgpass.png"))
         self.user_name = StringVar()
         self.usr_pass = StringVar()
         self.usr_type = StringVar()
@@ -28,7 +29,7 @@ class User_Login:
         logo.place(x=400,y=130,width=100,height=100)
         ims= Label(self.root,text="Inventory Management System",font =("Times New Roman",35),image=self.titlebg)
         ims.place(x=520,y=130,width=680,height=100)
-        self.login_frame = LabelFrame(self.root, text="LOGIN", bg = "white", font =("Arial",22))
+        self.login_frame = LabelFrame(self.root, text="LOGIN", bg = "white", font =("Times New Roman",22))
         self.login_frame.place(x = 400, y = 260, height = 350, width = 780)
         
         login_type = Label(self.login_frame, text = "   Login as : ",bg = "tomato", font =("Times New Roman",18)).grid(row = 0, column = 0,padx = 10, pady = 15,sticky="w")
@@ -40,12 +41,12 @@ class User_Login:
 
         login_id = Label(self.login_frame, text = " Username : ",bg = "tomato", font =("Times New Roman",18)).grid(row = 1, column = 0,padx = 10, pady = 15,sticky="w")
 
-        self.login_iden = Entry(self.login_frame, textvariable = self.user_name,bd="3",width = 45,bg = "LightGray", font =("Arial",18))
+        self.login_iden = Entry(self.login_frame, textvariable = self.user_name,bd="3",width = 48,bg = "LightGray", font =("Times New Roman",18))
         self.login_iden.grid(row = 1, column = 1,padx = 10, pady = 15,sticky="w")
 
         login_pass = Label(self.login_frame, text = "  Password : ",bg = "tomato", font =("Times New Roman",18)).grid(row = 2, column = 0,padx = 10, pady = 15,sticky="w")
 
-        self.login_passen = Entry(self.login_frame,textvariable =self.usr_pass ,show="*",bd="3", width = 49,bg = "LightGray", font =("Times New Roman",18))
+        self.login_passen = Entry(self.login_frame,textvariable =self.usr_pass ,show="*",bd="3", width = 48,bg = "LightGray", font =("Times New Roman",18))
         self.login_passen.grid(row = 2, column = 1,padx = 10, pady = 15,sticky="w")
 
         self.login_button = Button(self.login_frame, image=self.login_b, font =("Impact",14),bd="0", command = self.ureg_login)
@@ -88,61 +89,66 @@ class User_Login:
         if self.user_name.get() =="" or self.usr_pass.get() =="":
             messagebox.showerror("Error","All fields are mandatory",parent = self.root)
         else:
-            con = mysql.connector.connect(host="localhost",user="root",passwd="root",database="ims")
-            cur = con.cursor(buffered=True)
-            select = "SELECT * FROM USERS WHERE username = %s and password = %s"
-            value = (self.user_name.get(),self.usr_pass.get())
-            cur.execute(select,value)
-            rows = cur.fetchone()
-            if rows == NONE:
-                messagebox.showerror("Error","Invalid Credentials",parent = self.root)
-                self.usr_pass.set('')
-                self.user_name.set('')
-
+            if self.usr_type.get() == "Admin":
+                if self.user_name.get()=="ims@admin" and self.usr_pass.get() =="Admin_alas":
+                    self.root.destroy()
+                    import supplier
+                    supplier.run_supplier(self.user_name.get())
+                else:
+                    messagebox.showerror("Error","Incorrect login details!",parent = self.root)
+                    self.usr_pass.set('')
+                    self.user_name.set('')
             else:
-                if self.usr_type.get() == "Admin":
-                   self.root.destroy()
-                   import supplier
-                   supplier.run_supplier(self.user_name.get())
-                elif self.usr_type.get() == "User":
+                con = mysql.connector.connect(host="localhost",user="root",passwd="root",database="ims")
+                cur = con.cursor(buffered=True)
+                select = "SELECT * FROM USERS WHERE username = %s and password = %s"
+                value = (self.user_name.get(),self.usr_pass.get())
+                cur.execute(select,value)
+                rows = cur.fetchone()
+                if rows == NONE:
+                    messagebox.showerror("Error","Incorrect login details!",parent = self.root)
+                    self.usr_pass.set('')
+                    self.user_name.set('')
+                else:
                     self.root.destroy()
                     import buyer
                     buyer.run_buyer(self.user_name.get())
                    
-
-
-            con.commit()
-            con.close()
+                con.commit()
+                con.close()
             
     
     def ureg_forgotpass(self):
         self.root2 = Toplevel()
-        self.root2.geometry("580x430+550+200")
-        self.root2.config(bg="tomato")
+        self.root2.geometry("580x410+550+200")
+        self.root2.config(bg="white")
         self.root2.focus_force()
         self.root2.grab_set()
-        for_label = Label(self.root2, text = "PASSWORD RECOVERY", font =("Arial",20),bg="white")
-        for_label.place(x=130 ,y=30)
-        usrnew_id = Label(self.root2, text = "E-mail id  : ",bg = "white", font =("Arial",16))
-        usrnew_id.place(x=10 ,y=100)
-        self.usrnew_iden = Entry(self.root2, width = 30, font =("Arial",16),bd = "5")
-        self.usrnew_iden.place(x = 175, y = 100)
-        security = Label(self.root2, text = "Security Question : ", font =("Arial",16),bg = "white")
-        security.place(x=10 ,y=165)
-        self.security_combo = ttk.Combobox(self.root2, font =("Arial",16), state = "readonly" , width = 20, justify = "center")
+        for_label = Label(self.root2, image=self.passbg)
+        for_label.place(x=0 ,y=0)
+        titlelabel=Label(self.root2,text = "PASSWORD RECOVERY", bd=2,relief=SOLID,font =("Times New Roman",24,"bold"),bg="White", fg="Black")
+        titlelabel.place(x=5 ,y=5,relwidth=1,relheight=1)
+        titlelabel.pack(side=TOP,fill=X)
+        usrnew_id = Label(self.root2, text = "E-mail id  : ",bg="White", fg="Black", font =("Times New Roman",18))
+        usrnew_id.place(x=20 ,y=100)
+        self.usrnew_iden = Entry(self.root2, width = 30,bg = "LightGray", font =("Times New Roman",18),bd = "5")
+        self.usrnew_iden.place(x = 185, y = 100)
+        security = Label(self.root2, text = "Security Question : ", font =("Times New Roman",18),bg="White", fg="Black")
+        security.place(x=20 ,y=160)
+        self.security_combo = ttk.Combobox(self.root2, font =("Times New Roman",18), state = "readonly" , width = 25, justify = "center")
         self.security_combo['values']=("Select","Your favourite book","Your favourite movie","Your best friend")
-        self.security_combo.place(x=10 ,y=205)
+        self.security_combo.place(x=235 ,y=160)
         self.security_combo.current(0)
-        answer = Label(self.root2, text = "Answer    : ", font =("Arial",16),bg = "white",bd = "5")
-        answer.place(x=290 ,y=165)
-        self.answeren = Entry(self.root2, width = 20, font =("Arial",16),bd = "5")
-        self.answeren.place(x=290 ,y=205)
-        usrnew_pass = Label(self.root2, text = "New Password: ", font =("Arial",16),bg = "white")
-        usrnew_pass.place(x=10 ,y=280)
-        self.usrnew_passen = Entry(self.root2,show = "*", width = 30, font =("Arial",16),bd = "5")
-        self.usrnew_passen.place(x=175 ,y=280)
-        self.new_pass = Button(self.root2, text  = "Update Password", bd="0",font =("Arial",16), command = self.new_password)
-        self.new_pass.place(x = 200, y = 360)
+        answer = Label(self.root2, text = "Answer    : ", font =("Times New Roman",18),bg="White", fg="Black",bd = "5")
+        answer.place(x=20 ,y=220)
+        self.answeren = Entry(self.root2, width = 30,bg = "LightGray", font =("Times New Roman",18),bd = "5")
+        self.answeren.place(x=185 ,y=220)
+        usrnew_pass = Label(self.root2, text = "New Password: ", font =("Times New Roman",18),bg="White", fg="Black")
+        usrnew_pass.place(x=20 ,y=270)
+        self.usrnew_passen = Entry(self.root2,show = "*", width = 30,bg = "LightGray", font =("Times New Roman",18),bd = "5")
+        self.usrnew_passen.place(x=185 ,y=270)
+        self.new_pass = Button(self.root2, text  = "Update Password", bd="0",bg="White", fg="Black",font =("Times New Roman",18,"bold"), command = self.new_password)
+        self.new_pass.place(x = 200, y = 340)
         
 
     def register(self):
