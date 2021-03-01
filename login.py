5e3c2b6610a7e0d9daae2cc854394a35777dc0ed
unknown <naikamje19ie@student.mes.ac.in>
2021-03-01 10:34:44 +0530

    commit

--- login.py
+++ login.py
@@ -59,7 +59,7 @@ class User_Login:
         self.register_button.place(x=530, y =230, width = 200, height = 50)
 
     def new_password(self):
-        if self.usrnew_iden.get() =="" or self.answeren.get() =="" or self.usrnew_passen.get() =="":
+        if self.usrnew_iden.get() =="" or self.answeren.get() =="" or self.usrnew_passen.get() =="" or self.security_combo.get()=="Select":
             messagebox.showerror("Error","All fields are mandatory",parent = self.root2)
         else:
             try:
@@ -74,7 +74,8 @@ class User_Login:
                     value = (self.usrnew_passen.get(),self.usrnew_iden.get())
                     cur.execute(update,value)
                     messagebox.showinfo("Success","Password changed successfully!",parent = self.root2)
-                    self.root2.destroy()
+                    self.root.destroy()
+                    import login
                     con.commit()
 
                 con.commit()
@@ -116,39 +117,61 @@ class User_Login:
                    
                 con.commit()
                 con.close()
-            
+         
     
     def ureg_forgotpass(self):
+        
         self.root2 = Toplevel()
-        self.root2.geometry("580x410+550+200")
+        self.root2.geometry("1600x800+0+0")
         self.root2.config(bg="white")
         self.root2.focus_force()
         self.root2.grab_set()
-        for_label = Label(self.root2, image=self.passbg)
-        for_label.place(x=0 ,y=0)
-        titlelabel=Label(self.root2,text = "PASSWORD RECOVERY", bd=3,relief=SOLID,font =("Times New Roman",24,"bold"),bg="White", fg="Black")
-        titlelabel.place(x=5 ,y=5,relwidth=1,relheight=1)
-        titlelabel.pack(side=TOP,fill=X)
-        usrnew_id = Label(self.root2, text = "E-mail id  : ",bg="White", fg="Black", font =("Times New Roman",18))
-        usrnew_id.place(x=20 ,y=100)
-        self.usrnew_iden = Entry(self.root2, width = 30,bg = "LightGray", font =("Times New Roman",18),bd = "5")
-        self.usrnew_iden.place(x = 185, y = 100)
-        security = Label(self.root2, text = "Security Question : ", font =("Times New Roman",18),bg="White", fg="Black")
-        security.place(x=20 ,y=160)
-        self.security_combo = ttk.Combobox(self.root2, font =("Times New Roman",18), state = "readonly" , width = 25, justify = "center")
+        self.back= ImageTk.PhotoImage(Image.open("../IMS/icons/bglogin.jpg"))
+        back= Label(self.root2,image=self.back)
+        back.place(x=0,y=0,relwidth=1,relheight=1)
+        self.update_btn=ImageTk.PhotoImage(Image.open("../IMS/icons/button_update_pw.png"))
+        self.email_id= StringVar()
+        self.question=StringVar()
+        self.answer= StringVar()
+        self.new_pass= StringVar()
+        self.logo2= ImageTk.PhotoImage(Image.open("../IMS/icons/logo.jpg"))
+        logo2= Label(self.root2,image=self.logo2)
+        logo2.place(x=400,y=130,width=100,height=100)
+        ims2= Label(self.root2,text="Inventory Management System",font =("Times New Roman",35),image=self.titlebg)
+        ims2.place(x=520,y=130,width=680,height=100)
+        self.root2 = LabelFrame(self.root2, text="RECOVER PASSWORD", bg = "white", font =("Times New Roman",22))
+        self.root2.place(x = 400, y = 260, height = 380, width = 780)
+
+        usrnew_id = Label(self.root2, text = "         Email id       :",bg = "tomato", font =("Times New Roman",18)).grid(row = 0, column = 0,padx = 10, pady = 15,sticky="w")
+
+        self.usrnew_iden = Entry(self.root2, textvariable = self.email_id,bd="3",width = 43,bg = "LightGray", font =("Times New Roman",18))
+        self.usrnew_iden.grid(row = 0, column = 1,padx = 10, pady = 15,sticky="w")
+        
+        security = Label(self.root2, text = " Security question :",bg = "tomato", font =("Times New Roman",18)).grid(row = 1, column = 0,padx = 10, pady = 15,sticky="w")
+
+        self.security_combo = ttk.Combobox(self.root2,textvariable = self.question, font =("Times New Roman",18), width = 41,state = "readonly" , justify = "center")
         self.security_combo['values']=("Select","Your favourite book","Your favourite movie","Your best friend")
-        self.security_combo.place(x=235 ,y=160)
+        self.security_combo.grid(row = 1, column = 1,padx = 10, pady = 15,sticky="w")
         self.security_combo.current(0)
-        answer = Label(self.root2, text = "Answer    : ", font =("Times New Roman",18),bg="White", fg="Black",bd = "5")
-        answer.place(x=20 ,y=220)
-        self.answeren = Entry(self.root2, width = 30,bg = "LightGray", font =("Times New Roman",18),bd = "5")
-        self.answeren.place(x=185 ,y=220)
-        usrnew_pass = Label(self.root2, text = "New Password: ", font =("Times New Roman",18),bg="White", fg="Black")
-        usrnew_pass.place(x=20 ,y=270)
-        self.usrnew_passen = Entry(self.root2,show = "*", width = 30,bg = "LightGray", font =("Times New Roman",18),bd = "5")
-        self.usrnew_passen.place(x=185 ,y=270)
-        self.new_pass = Button(self.root2, text  = "Update Password", bd=3,bg="White", fg="Black",font =("Times New Roman",18,"bold"), command = self.new_password)
-        self.new_pass.place(x = 200, y = 340)
+
+        answer = Label(self.root2, text = "        Answer         :",bg = "tomato", font =("Times New Roman",18)).grid(row = 2, column = 0,padx = 10, pady = 15,sticky="w")
+
+        self.answeren = Entry(self.root2, textvariable = self.answer,bd="3",width = 43,bg = "LightGray", font =("Times New Roman",18))
+        self.answeren.grid(row = 2, column = 1,padx = 10, pady = 15,sticky="w")
+
+        usrnew_pass = Label(self.root2, text = "   New Password   :",bg = "tomato", font =("Times New Roman",18)).grid(row = 3, column = 0,padx = 10, pady = 15,sticky="w")
+
+        self.usrnew_passen = Entry(self.root2, textvariable = self.new_pass,bd="3",width = 43,bg = "LightGray", font =("Times New Roman",18))
+        self.usrnew_passen.grid(row = 3, column = 1,padx = 10, pady = 15,sticky="w")
+        
+        #self.new_pass = Button(self.root2, image=self.login_b, font =("Impact",14),bd="0", command = self.new_password)
+        #self.new_pass = Button(self.root2, text  = "Update Password", bd="0",bg="Red", fg="White",font =("Times New Roman",18,"bold"), command = self.new_password)
+        #self.new_pass.place(x=305, y = 280, width = 200, height = 42)
+        self.new_pass = Button(self.root2, image=self.update_btn, font =("Impact",14),bd=3,bg="red2", command = self.new_password)
+        self.new_pass.place(x=305, y = 280, width = 200, height = 42)
+
+       
+
         
 
     def register(self):
